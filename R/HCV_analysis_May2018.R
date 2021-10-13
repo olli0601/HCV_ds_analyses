9d256faff7c9dc8bd88df94bac86e24868e717b4
Oliver Ratmann <monsieur.olli@gmail.com>
2021-10-13 10:23:48 +0100

    latest update

--- R/HCV_analysis_May2018.R
+++ R/HCV_analysis_May2018.R
@@ -1,3 +1,98 @@
+cmd.docker.iva <- function(sample.id,fastq.fwd,fastq.bwd,file.adapters,file.primers,prog.docker.iva,prog.docker.trimmomatic,dir.inputs,dir.fastq,dir.contigs,smalt.k,smalt.s,smalt.y)
+{
+	cmd <- ''
+	cmd <- paste0(cmd,"CWD=$(pwd)\n")
+	cmd <- paste0(cmd,"SMALT_K=",smalt.k,"\n")
+	cmd <- paste0(cmd,"SMALT_S=",smalt.s,"\n")
+	cmd <- paste0(cmd,"SMALT_ID=",smalt.y,"\n")
+	cmd <- paste0(cmd,"DIR_INPUTS=",dir.inputs,"\n")
+	cmd <- paste0(cmd,"DIR_FASTQ=",dir.fastq,"\n")		
+	cmd <- paste0(cmd,"DIR_CONTIGS=",dir.contigs,"\n")
+	cmd <- paste0(cmd,"SAMPLE_ID=",sample.id,"\n")
+	cmd <- paste0(cmd,"DIR_CONTIGS_SAMPLE=${DIR_CONTIGS}/${SAMPLE_ID}\n")	
+	cmd <- paste0(cmd,"FASTQ_FWD=",fastq.fwd,"\n")
+	cmd <- paste0(cmd,"FASTQ_BWD=",fastq.bwd,"\n")
+	cmd <- paste0(cmd,"mkdir -p ${DIR_CONTIGS_SAMPLE}\n")	
+	cmd <- paste0(cmd,"cp -n ",file.adapters," ${DIR_CONTIGS_SAMPLE}/adapters.fasta\n")
+	cmd <- paste0(cmd,"cp -n ",file.primers," ${DIR_CONTIGS_SAMPLE}/primers.fasta\n")	
+	cmd <- paste0(cmd,"cp -n ${DIR_FASTQ}/${FASTQ_FWD} ${DIR_CONTIGS_SAMPLE}/${FASTQ_FWD}\n")
+	cmd <- paste0(cmd,"cp -n ${DIR_FASTQ}/${FASTQ_BWD} ${DIR_CONTIGS_SAMPLE}/${FASTQ_BWD}\n")
+	tmp <- paste0("docker run --rm -it",
+			" -v ${DIR_CONTIGS_SAMPLE}:/data"," ",
+			prog.docker.iva, " ",
+			"--trimmomatic ",prog.docker.trimmomatic," ",
+			"--adapters /data/adapters.fasta ",
+			"--pcr_primers /data/primers.fasta ",
+			"--smalt_k ${SMALT_K} ",
+			"--smalt_s ${SMALT_S} ",
+			"--smalt_id ${SMALT_ID} ",
+			"-f /data/${FASTQ_FWD} ",
+			"-r /data/${FASTQ_BWD} ",
+			"/data/iva")
+	cmd <- paste0(cmd,tmp,"\n")
+	cmd <- paste0(cmd,"mv ${DIR_CONTIGS_SAMPLE}/iva/contigs.fasta ${DIR_CONTIGS}/${SAMPLE_ID}_contigs.fasta\n") 
+	cmd <- paste0(cmd,"rm -rf ${DIR_CONTIGS_SAMPLE}\n")
+	cmd
+}
+
+cmd.shiver.align.contigs <- function(sample.id, contigs, file.config, prog.shiver.align, dir.init, dir.inputs, dir.contigs, dir.out)
+{		
+	cmd <- ''
+	cmd <- paste0(cmd,"CWD=$(pwd)\n")
+	cmd <- paste0(cmd,"echo $CWD\n")
+	cmd <- paste0(cmd,"SAMPLE_ID=",sample.id,"\n")
+	cmd <- paste0(cmd,"DIR_INIT=",dir.init,"\n")
+	cmd <- paste0(cmd,"DIR_INPUTS=",dir.inputs,"\n")
+	cmd <- paste0(cmd,"DIR_CONTIGS=",dir.contigs,"\n")
+	cmd <- paste0(cmd,"FILE_CONTIGS=",file.path('${DIR_CONTIGS}',contigs),"\n")
+	cmd <- paste0(cmd,"DIR_OUT=",dir.out,"\n")
+	cmd <- paste0(cmd,"DIR_OUT_SAMPLE=${DIR_OUT}/${SAMPLE_ID}\n")
+	cmd <- paste0(cmd,"mkdir -p ${DIR_OUT_SAMPLE}\n")
+	cmd <- paste0(cmd,"cd ${DIR_OUT_SAMPLE}\n")
+	tmp <- paste0(
+			prog.shiver.align,' ',
+			'${DIR_INIT}',' ',
+			file.config,' ',			
+			'${FILE_CONTIGS} ',
+			'${SAMPLE_ID}')
+	cmd <- paste0(cmd,tmp,"\n")
+	cmd <- paste0(cmd,"cd $CWD\n")
+	cmd
+}
+
+cmd.shiver.map.contigs <- function(sample.id,fastq.fwd,fastq.bwd,contigs,ali.contigs,file.config,prog.shiver.map,dir.init, dir.inputs, dir.fastq,dir.contig,dir.out)
+{
+	cmd <- ''
+	cmd <- paste0(cmd,"CWD=$(pwd)\n")
+	cmd <- paste0(cmd,"echo $CWD\n")		
+	cmd <- paste0(cmd,"DIR_INIT=",dir.init,"\n")
+	cmd <- paste0(cmd,"DIR_INPUTS=",dir.inputs,"\n")
+	cmd <- paste0(cmd,"DIR_FASTQ=",dir.fastq,"\n")
+	cmd <- paste0(cmd,"DIR_CONTIGS=",dir.contigs,"\n")
+	cmd <- paste0(cmd,"DIR_OUT=",dir.out,"\n")		
+	cmd <- paste0(cmd,"SAMPLE_ID=",sample.id,"\n")
+	cmd <- paste0(cmd,"FASTQ_FWD=",fastq.fwd,"\n")
+	cmd <- paste0(cmd,"FASTQ_BWD=",fastq.bwd,"\n")
+	cmd <- paste0(cmd,"FILE_CONTIGS=",contigs,"\n")
+	cmd <- paste0(cmd,"FILE_ALI_CONTIGS=",ali.contigs,"\n")
+	cmd <- paste0(cmd,"DIR_OUT_SAMPLE=${DIR_OUT}/${SAMPLE_ID}\n")
+	cmd <- paste0(cmd,"cd ${DIR_OUT_SAMPLE}\n")
+	tmp <- paste0(
+			prog.shiver.map,' ',
+			'${DIR_INIT}',' ',
+			file.config,' ',
+			'${DIR_CONTIGS}/${FILE_CONTIGS}',' ',
+			'${SAMPLE_ID} ',
+			'${SAMPLE_ID}.blast ',
+			'${FILE_ALI_CONTIGS} ',
+			'${DIR_FASTQ}/${FASTQ_FWD} ',
+			'${DIR_FASTQ}/${FASTQ_BWD}'
+	)		
+	cmd <- paste0(cmd,tmp,"\n")	
+	cmd <- paste0(cmd,"cd $CWD\n")
+	cmd
+}
+
 May2018_v210224 <- function()
 {	
 	
@@ -6,6 +101,8 @@ May2018_v210224 <- function()
 	prog.iva <- 'iva'
 	prog.docker.iva <- 'sangerpathogens/iva iva'
 	prog.docker.trimmomatic <- '/Trimmomatic-0.38/trimmomatic-0.38.jar'
+	prog.shiver.align <- file.path(dir.shiver, 'shiver_align_contigs.sh')
+	prog.shiver.map <- file.path(dir.shiver, 'shiver_map_reads.sh')
 	smalt.k <- "15"
 	smalt.s <- "3"
 	smalt.y <- "0.7"
@@ -13,13 +110,12 @@ May2018_v210224 <- function()
 	regex.forward.id <- '(.*)_R1_[0-9]+.fastq.gz'
 	regex.backward.id <- '(.*)_R2_[0-9]+.fastq.gz'
 	
-	dir.project.base <- '/Users/or105/Box/OR_Work/2021/2021_HCV/HCV1_210224_'
+	dir.project.base <- '/Users/or105/sandbox/HCV/HCV1_210224_'
 	dir.init <- paste0(dir.project.base,'init_dir')
 	dir.inputs <- paste0(dir.project.base,'settings')
 	dir.contigs <- paste0(dir.project.base,'contigs')
 	dir.fastq <- paste0(dir.project.base,'fastq')
-	dir.out <- paste0(dir.project.base,'results')
-	
+	dir.out <- paste0(dir.project.base,'results')	
 	
 	#
 	#	initialise project
@@ -46,10 +142,10 @@ May2018_v210224 <- function()
 	cat(cmd)
 	
 	#
-	#	read data to be processed
+	#	make contigs with IVA
+	#	read data to be processed	
 	#
-	
-	df <- data.table(F=list.files(dir.fastq))
+	df <- data.table(F=list.files(dir.fastq, pattern='*.fastq.gz$'))
 	df[, SAMPLE_ID := gsub(regex.sample.id,'\\1',F)]
 	df[, DIRECTION := NA_character_]
 	tmp <- df[,which(grepl(regex.forward.id,F))]
@@ -63,130 +159,129 @@ May2018_v210224 <- function()
 		warning('Did not find bwd and fwd fastq for sample IDs. Excluding ', tmp2)
 	df <- merge(df, subset(tmp, n==2, SAMPLE_ID), by='SAMPLE_ID')	
 	df <- dcast.data.table(df, SAMPLE_ID~DIRECTION, value.var='F')
-	
-	
-	#
-	#	make contigs with IVA
-	#
-	
-	data.dir <- "/Users/or105/Box/OR_Work/2021/2021_HCV/HCV1_210224_results/test"
-	out.dir <- "docker_iva_out"
-	cmd <- ''
-	cmd <- paste0(cmd,"docker run --rm -it -v ",data.dir,":/data sangerpathogens/iva ")
-	cmd <- paste0(cmd,"iva -f /data/reads_1.fq.gz -r /data/reads_2.fq.gz /data/",out.dir)
-	
-	i <- 1
-		 
-	cmd <- ''
-	cmd <- paste0(cmd,"CWD=$(pwd)\n")
-	cmd <- paste0(cmd,"echo $CWD\n")
-	cmd <- paste0(cmd,"SAMPLE_ID=",df$SAMPLE_ID[i],"\n")
-	cmd <- paste0(cmd,"DIR_INPUTS=",dir.inputs,"\n")
-	cmd <- paste0(cmd,"DIR_FASTQ=",dir.fastq,"\n")		
-	cmd <- paste0(cmd,"DIR_CONTIGS=",dir.contigs,"\n")			
-	cmd <- paste0(cmd,"DIR_CONTIGS_SAMPLE=${DIR_CONTIGS}/${SAMPLE_ID}\n")	
-	cmd <- paste0(cmd,"FASTQ_FWD=",df$fastq_fwd[i],"\n")
-	cmd <- paste0(cmd,"FASTQ_BWD=",df$fastq_bwd[i],"\n")
-	cmd <- paste0(cmd,"mkdir -p ${DIR_CONTIGS_SAMPLE}\n")	
-	cmd <- paste0(cmd,"cp -n ",file.adapters," ${DIR_CONTIGS_SAMPLE}/adapters.fasta\n")
-	cmd <- paste0(cmd,"cp -n ",file.primers," ${DIR_CONTIGS_SAMPLE}/primers.fasta\n")	
-	cmd <- paste0(cmd,"cp -n ${DIR_FASTQ}/${FASTQ_FWD} ${DIR_CONTIGS_SAMPLE}/${FASTQ_FWD}\n")
-	cmd <- paste0(cmd,"cp -n ${DIR_FASTQ}/${FASTQ_BWD} ${DIR_CONTIGS_SAMPLE}/${FASTQ_BWD}\n")
-	tmp <- paste0("docker run --rm -it",
-			" -v ${DIR_CONTIGS_SAMPLE}:/data"," ",
-			prog.docker.iva, " ",
-			"--trimmomatic ",prog.docker.trimmomatic," ",
-			"--adapters /data/adapters.fasta ",
-			"--pcr_primers /data/primers.fasta ",
-			"--smalt_k ",smalt.k," ",
-			"--smalt_s ",smalt.s," ",
-			"--smalt_id ",smalt.y," ",
-			"-f /data/${FASTQ_FWD} ",
-			"-r /data/${FASTQ_BWD} ",
-			"/data/iva")
-	cmd <- paste0(cmd,tmp,"\n")
-	cmd <- paste0(cmd,"mv ${DIR_CONTIGS_SAMPLE}/iva/contigs.fasta ${DIR_CONTIGS}/${SAMPLE_ID}_contigs.fasta\n") 
-	cmd <- paste0(cmd,"rm -rf ${DIR_CONTIGS_SAMPLE}\n")
-	cmd <- paste0(cmd,"cd $CWD\n")
-	cat(cmd)
-	
-	tmp <- paste0(prog.iva," ",
-		"--trimmomatic ",prog.trimmomatic," ",
-		"--adapters ",file.adapters," ",
-		"--pcr_primers ",file.primers," ",
-		"--smalt_k ",smalt.k," ",
-		"--smalt_s ",smalt.s," ",
-		"--smalt_id ",smalt.y," ",
-		"-f ${DIR_FASTQ}/${FASTQ_FWD} ",
-		"-r ${DIR_FASTQ}/${FASTQ_BWD} ",
-		"${DIR_OUT}")
-	cmd <- paste0(cmd,tmp,"\n")
-	cmd <- paste0(cmd,"cd $CWD\n")
-	cat(cmd)
+	#	build cmd	
+	cmds <- vector('list', nrow(df))
+	for(i in 1:nrow(df))
+	{		
+		cmd <- cmd.docker.iva(df$SAMPLE_ID[i],df$fastq_fwd[i],df$fastq_bwd[i],
+				file.adapters,file.primers,
+				prog.docker.iva,prog.docker.trimmomatic,
+				dir.inputs,dir.fastq,dir.contigs,
+				smalt.k,smalt.s,smalt.y)
+		cmds[[i]] <- cmd
+	}
+	cmd <- paste0(cmds, collapse='\n#\n#      MAKE CONTIGS FOR SAMPLE\n#\n')
+	cat(cmd, file=file.path(dir.out,'make_contigs.sh'))	
+	system(paste0('chmod a+x ',file.path(dir.out,'make_contigs.sh')))
 	
 	#
-	#	process contigs
-	#
-	
-	sample.id <- 'MysteryHIV'
-	file.contigs <- file.path('${DIR_CONTIGS}','${SAMPLE_ID}_contigs.fasta')	
-	dir.out.sample <- file.path(dir.out, '${SAMPLE_ID}')
-	
-	cmd <- ''
-	cmd <- paste0(cmd,"CWD=$(pwd)\n")
-	cmd <- paste0(cmd,"echo $CWD\n")
-	cmd <- paste0(cmd,"SAMPLE_ID=",sample.id,"\n")
-	cmd <- paste0(cmd,"DIR_INIT=",dir.init,"\n")
-	cmd <- paste0(cmd,"DIR_INPUTS=",dir.inputs,"\n")
-	cmd <- paste0(cmd,"DIR_CONTIGS=",dir.contigs,"\n")
-	cmd <- paste0(cmd,"mkdir -p ", dir.out.sample, "\n")
-	cmd <- paste0(cmd,"cd ", dir.out.sample, "\n")
-	tmp <- paste0(
-			file.path(dir.shiver, 'shiver_align_contigs.sh'),' ',
-			'${DIR_INIT}',' ',
-			file.config,' ',
-			file.contigs,' ',
-			'${SAMPLE_ID}')
-	cmd <- paste0(cmd,tmp,"\n")
-	cmd <- paste0(cmd,"cd $CWD\n")
-	cat(cmd)
-	system(cmd)
+	#	align IVA contigs against refs
+	#	read data to be processed
+	#	
+	df <- data.table(F=list.files(dir.fastq, pattern='*.fastq.gz$'))
+	df[, SAMPLE_ID := gsub(regex.sample.id,'\\1',F)]
+	df[, DIRECTION := NA_character_]
+	tmp <- df[,which(grepl(regex.forward.id,F))]
+	set(df, tmp, 'DIRECTION', 'fastq_fwd')
+	tmp <- df[,which(grepl(regex.backward.id,F))]
+	set(df, tmp, 'DIRECTION', 'fastq_bwd')
+	stopifnot( df[, !any(is.na(DIRECTION))] )
+	tmp <- df[, list(n=length(DIRECTION)), by='SAMPLE_ID']
+	tmp2 <- subset(tmp, n!=2)[, unique(SAMPLE_ID)]
+	if(length(tmp2))
+		warning('Did not find bwd and fwd fastq for sample IDs. Excluding ', tmp2)
+	df <- merge(df, subset(tmp, n==2, SAMPLE_ID), by='SAMPLE_ID')	
+	df <- dcast.data.table(df, SAMPLE_ID~DIRECTION, value.var='F')
+	#	read contigs
+	tmp <- data.table(contigs=list.files(dir.contigs, pattern='*_contigs.fasta$'))
+	tmp[, SAMPLE_ID := gsub('(.+)_contigs.fasta','\\1',contigs)]
+	df <- merge(df, tmp, by='SAMPLE_ID')	
+	#	build cmd
+	cmds <- vector('list', nrow(df))
+	for(i in 1:nrow(df))
+	{
+		cmd <- cmd.shiver.align.contigs(
+			df$SAMPLE_ID[i], 
+			df$contigs[i], 
+			file.config, 
+			prog.shiver.align, 
+			dir.init, 
+			dir.inputs, 
+			dir.contigs, 
+			dir.out)
+		cmds[[i]] <- cmd
+	}
+	cmd <- paste0(cmds, collapse='\n#\n#      ALIGN CONTIGS FOR SAMPLE\n#\n')
+	cat(cmd, file=file.path(dir.out,'align_contigs.sh'))
+	system(paste0('chmod a+x ',file.path(dir.out,'align_contigs.sh')))
 	
 	#
 	#	continue mapping
 	#
-	file.blast <- '${SAMPLE_ID}.blast'
-	file.processed.contigs <- '${SAMPLE_ID}_cut_wRefs.fasta'
-	file.forward.fastq <- file.path('${DIR_FASTQ}', '${SAMPLE_ID}_1.fastq')
-	file.backward.fastq <- file.path('${DIR_FASTQ}', '${SAMPLE_ID}_2.fastq')
+	df <- data.table(F=list.files(dir.fastq, pattern='*.fastq.gz$'))
+	df[, SAMPLE_ID := gsub(regex.sample.id,'\\1',F)]
+	df[, DIRECTION := NA_character_]
+	tmp <- df[,which(grepl(regex.forward.id,F))]
+	set(df, tmp, 'DIRECTION', 'fastq_fwd')
+	tmp <- df[,which(grepl(regex.backward.id,F))]
+	set(df, tmp, 'DIRECTION', 'fastq_bwd')
+	stopifnot( df[, !any(is.na(DIRECTION))] )
+	tmp <- df[, list(n=length(DIRECTION)), by='SAMPLE_ID']
+	tmp2 <- subset(tmp, n!=2)[, unique(SAMPLE_ID)]
+	if(length(tmp2))
+		warning('Did not find bwd and fwd fastq for sample IDs. Excluding ', tmp2)
+	df <- merge(df, subset(tmp, n==2, SAMPLE_ID), by='SAMPLE_ID')	
+	df <- dcast.data.table(df, SAMPLE_ID~DIRECTION, value.var='F')
+	#	read contigs
+	tmp <- data.table(contigs=list.files(dir.contigs, pattern='*_contigs.fasta$'))
+	tmp[, SAMPLE_ID := gsub('(.+)_contigs.fasta','\\1',contigs)]
+	df <- merge(df, tmp, by='SAMPLE_ID')	
+	#	read aligned contigs
+	tmp <- data.table(ali_contigs_raw=list.files(dir.out, pattern='*_raw_wRefs.fasta$', recursive=TRUE))
+	tmp[, SAMPLE_ID := dirname(ali_contigs_raw)]
+	df <- merge(df, tmp, by='SAMPLE_ID', all.x=TRUE)	
+	tmp <- data.table(ali_contigs_cut=list.files(dir.out, pattern='*_cut_wRefs.fasta$', recursive=TRUE))
+	tmp[, SAMPLE_ID := dirname(ali_contigs_cut)]
+	df <- merge(df, tmp, by='SAMPLE_ID', all.x=TRUE)	
+	df[, ali_contigs_sel := ali_contigs_raw]
+	tmp <- df[, which(!is.na(ali_contigs_cut))]
+	set(df, tmp, 'ali_contigs_sel', df[tmp, ali_contigs_cut])
+	df <- subset(df, !is.na(ali_contigs_sel))
+	set(df, NULL, 'ali_contigs_sel', df[,basename(ali_contigs_sel)])
+	#	build cmd
+	cmds <- vector('list', nrow(df))
+	for(i in 1:nrow(df))
+	{
+		cmd <- cmd.shiver.map.contigs(
+			df$SAMPLE_ID[i],
+			df$fastq_fwd[i],
+			df$fastq_bwd[i],
+			df$contigs[i],
+			df$ali_contigs_sel[i],
+			file.config,
+			prog.shiver.map,
+			dir.init, 
+			dir.inputs, 
+			dir.fastq,
+			dir.contig,
+			dir.out)
+		cmds[[i]] <- cmd
+	}
+	cmd <- paste0(cmds, collapse='\n#\n#      MAP READS\n#\n')
+	cat(cmd, file=file.path(dir.out,'map_reads.sh'))
+	system(paste0('chmod a+x ',file.path(dir.out,'map_reads.sh')))	
+}
+
+inspect.fastq <- function()
+{
+	library(ShortRead)
+	file <- '~/sandbox/HCV/HCV1_210224_results/010519-M10_S10_L001/010519-M10_S10_L001_R1_001.fastq'
+	reads <- readFastq(file)
+	sr <- sread(reads)
+	sri <- id(reads)
 	
-	cmd <- ''
-	cmd <- paste0(cmd,"CWD=$(pwd)\n")
-	cmd <- paste0(cmd,"echo $CWD\n")	
-	cmd <- paste0(cmd,"SAMPLE_ID=",sample.id,"\n")
-	cmd <- paste0(cmd,"DIR_INIT=",dir.init,"\n")
-	cmd <- paste0(cmd,"DIR_INPUTS=",dir.inputs,"\n")
-	cmd <- paste0(cmd,"DIR_FASTQ=",dir.fastq,"\n")
-	cmd <- paste0(cmd,"DIR_CONTIGS=",dir.contigs,"\n")	
-	cmd <- paste0(cmd,"cd ", dir.out.sample, "\n")
-	tmp <- paste0(
-			file.path(dir.shiver, 'shiver_map_reads.sh'),' ',
-			'${DIR_INIT}',' ',
-			file.config,' ',
-			file.contigs,' ',
-			'${SAMPLE_ID}', ' ',
-			file.blast, ' ',
-			file.processed.contigs, ' ',
-			file.forward.fastq, ' ',
-			file.backward.fastq
-	)
-	cmd <- paste0(cmd,tmp,"\n")	
-	cmd <- paste0(cmd,"cd $CWD\n")
-	cat(cmd)
-	
-	#	need trimmomatic
-	#	script files will be easier to read if I define env variables with dir names
-	'~/shiver/shiver_map_reads.sh MyInitDir config.sh contigs.fasta SID \
-			SID.blast  SID_cut_wRefs.fasta  reads_1.fastq.gz  reads_2.fastq.gz
-			'
+	file2 <- '~/sandbox/HCV/HCV1_210224_results/010519-M10_S10_L001/010519-M10_S10_L001_R2_001.fastq'
+	reads2 <- readFastq(file)
+	sr2 <- sread(reads2)
+	sri2 <- id(reads2)
 }
